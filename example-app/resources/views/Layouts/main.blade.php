8f2b0de50ccbbf5c1fdeff57bf84a46d3580458b
Nouman-Shahid <noumanshahid2486@gmail.com>
2024-08-08 18:48:13 +0500

    code refactoring

--- example-app/resources/views/Layouts/main.blade.php
+++ example-app/resources/views/Layouts/main.blade.php
@@ -14,7 +14,7 @@
         <a href="#" class="-m-1.5 p-1.5">
           <span class="sr-only">Your Company</span>
           <h2 class="font-bold text-[3vh]">Laravel</h2>
-        </a>
+        </a> 
       </div>
       <div class="flex lg:hidden">
         <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
