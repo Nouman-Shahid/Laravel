e6ccf59aff477603b6fbf997489d4b51d88f47a2
Nouman-Shahid <noumanshahid2486@gmail.com>
2024-08-15 14:09:59 +0500

    code refactoring

--- Project-DB/routes/web.php
+++ Project-DB/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DoctorController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [DoctorController::class,'showDoctor']);
