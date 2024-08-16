73cb8d1e35b1696ceb0baf16655100aab138fbe8
Nouman-Shahid <noumanshahid2486@gmail.com>
2024-08-16 13:15:51 +0500

    code refactoring

--- Project/resources/views/teachers.blade.php
+++ Project/resources/views/teachers.blade.php
@@ -1,19 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-@foreach ($data as $item => $teachers)
 
-           <tr style="border: 2px solid red">
-               <td>{{$teachers->name}}|</td>
-               <td>{{$teachers->phone}}|</td>
-               <td>{{$teachers->email}}|</td>
-               <td>{{$teachers->city}}| </td>
-           </tr>
-            @endforeach</body>
+<body>
+    <table>
+        @foreach ($data as $item => $teachers)
+            <tr style="border: 2px solid red">
+                <td>{{ $teachers->name }}|</td>
+                <td>{{ $teachers->phone }}|</td>
+                <td>{{ $teachers->email }}|</td>
+                <td>{{ $teachers->city }}| </td>
+            </tr>
+        @endforeach
+    </table>
+</body>
+
 </html>
