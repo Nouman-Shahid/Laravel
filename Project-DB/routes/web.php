b0abef2065ead93d25c14371e8e907e703bba21e
Nouman-Shahid <noumanshahid2486@gmail.com>
2024-08-17 13:51:11 +0500

    code refactoring

--- Project-DB/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DoctorController;
-
-Route::get('/', [DoctorController::class,'showDoctor']);
-Route::get('/user/{id}', [DoctorController::class,'singleUser'])->name('view.user');
