37c8fbd3682edafb487769ecac9b3721d2931ad1
Nouman-Shahid <noumanshahid2486@gmail.com>
2024-12-11 14:46:39 +0500

    Add Authentication Logic

--- Seekio_Frontend/src/App.jsx
+++ Seekio_Frontend/src/App.jsx
@@ -1,7 +1,11 @@
 function App() {
+  const redirectToLogin = () => {
+    window.location.href = "http://127.0.0.1:8000/login";
+  };
+
   return (
     <>
-      <p>Hello</p>
+      <button onClick={redirectToLogin}>Login</button>;
     </>
   );
 }
