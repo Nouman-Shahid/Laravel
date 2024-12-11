cdbd064092ad86b3ae656df2bb4e7207df70b84f
Nouman-Shahid <noumanshahid2486@gmail.com>
2024-12-11 17:51:24 +0500

    Refine Authentication Logic

--- Seekio_Frontend/src/main.jsx
+++ Seekio_Frontend/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <Auth0Provider
+    domain="tick-task.au.auth0.com"
+    clientId="g6W2lkrFel6aKB6wsJbkeCdXktHMKbI4"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
     <App />
-  </StrictMode>,
-)
+  </Auth0Provider>
+);
